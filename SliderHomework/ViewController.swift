8ebd1d550c825a17b767771e5ee3112a5a0c6c07
KonstantinKonstantinov <konstantinov87@mail.ru>
2022-03-06 13:38:46 +0300

    Sliders

--- SliderHomework/ViewController.swift
+++ SliderHomework/ViewController.swift
@@ -9,11 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var viewColor: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        viewColor.backgroundColor = .white
     }
 
-
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(redSlider.value)
+         var redColorValue = CGFloat(redSlider.value)
+        
+        var greenColorValue = CGFloat(greenSlider.value)
+        
+        var blueColorValue = CGFloat(blueSlider.value)
+        
+        var color = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+        
+          self.viewColor.backgroundColor = color
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(greenSlider.value)
+        var redColorValue = CGFloat(redSlider.value)
+       
+       var greenColorValue = CGFloat(greenSlider.value)
+       
+       var blueColorValue = CGFloat(blueSlider.value)
+       
+       var color = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+       
+         self.viewColor.backgroundColor = color
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(blueSlider.value)
+        var redColorValue = CGFloat(redSlider.value)
+       
+       var greenColorValue = CGFloat(greenSlider.value)
+       
+       var blueColorValue = CGFloat(blueSlider.value)
+       
+       var color = UIColor(red: redColorValue, green: greenColorValue, blue: blueColorValue, alpha: 1)
+       
+         self.viewColor.backgroundColor = color
+    }
+    
+   
 }
+    
+
 
